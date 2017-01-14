08eba405015d37c560726d9d35e28ccb69455200
Mark Waite <mark.earl.waite@gmail.com>
2017-01-13 20:02:02 -0700

    Remove trailing white space from a few lines
    Fix javadoc comment in CheckoutCommand

--- src/main/java/org/jenkinsci/plugins/gitclient/CheckoutCommand.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/CheckoutCommand.java
@@ -49,11 +49,11 @@ public interface CheckoutCommand extends GitCommand {
      * @return a {@link org.jenkinsci.plugins.gitclient.CheckoutCommand} object.
      */
     CheckoutCommand timeout(Integer timeout);
-    
+
     /**
      * Call "git lfs pull" for the given remote after checkout.
-     * 
-     * @param remote a {@link java.lang.String} object.
+     *
+     * @param lfsRemote the remote used for checkout
      * @return a {@link org.jenkinsci.plugins.gitclient.CheckoutCommand} object.
      */
     CheckoutCommand lfsRemote(String lfsRemote);
