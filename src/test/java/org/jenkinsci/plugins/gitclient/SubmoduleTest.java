1dce18d3d6135311d85a017cf3566b890709f208
Mark Waite <mark.earl.waite@gmail.com>
2017-03-12 23:12:17 -0600

    Add concept of finalBranch to SubmoduleTest
    Includes comments which highlight collisions between different initial
    branch and final branch definitions.  For example, transitions from
    tests/getSubmodules to tests/nonSubmodules fails with conflicting files
    in JGit.
    
    Needs much more work, including
      - cache submodules locally for test speed
      - test more submodule commands
      - test the clean command
      - test modifying tracked files
      - test modifying untracked files

--- src/test/java/org/jenkinsci/plugins/gitclient/SubmoduleTest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/SubmoduleTest.java
@@ -1,30 +1,30 @@
 package org.jenkinsci.plugins.gitclient;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import org.junit.After;
-import static org.junit.Assert.*;
+import org.eclipse.jgit.lib.ObjectId;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.jvnet.hudson.test.TemporaryDirectoryAllocator;
 
 @RunWith(Parameterized.class)
 public class SubmoduleTest {
 
-    private final TemporaryDirectoryAllocator temporaryDirectoryAllocator;
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     private final String gitImplementation;
     private final String initialBranch;
+    private final String finalBranch;
 
-    public SubmoduleTest(String implementation, String initialBranch) {
-        temporaryDirectoryAllocator = new TemporaryDirectoryAllocator();
+    public SubmoduleTest(String implementation, String initialBranch, String finalBranch) {
         gitImplementation = implementation;
         this.initialBranch = initialBranch;
+        this.finalBranch = finalBranch;
     }
 
     private LocalWorkspace ws;
@@ -54,28 +54,21 @@ public class SubmoduleTest {
      */
     @Before
     public void createWorkspace() throws Exception {
-        File repo = temporaryDirectoryAllocator.allocate();
-        ws = new LocalWorkspace(repo, gitImplementation);
+        /* Should copy ntp, firewall, and sshkeys repos into a reference repo */
+        ws = new LocalWorkspace(tempFolder.newFolder(), gitImplementation);
     }
 
-    @After
-    public void removeWorkspace() throws Exception {
-        try {
-            temporaryDirectoryAllocator.dispose();
-        } catch (IOException e) {
-            e.printStackTrace(System.err);
-        }
-    }
-
-    @Parameterized.Parameters(name = "impl:{0} initial-branch:{1}")
+    @Parameterized.Parameters(name = "impl:{0} start:{1} end:{2}")
     public static Collection testScenarios() {
-        List<Object[]> testArgs = new ArrayList<Object[]>();
-        String[] gitImplementations = new String[]{"git", "jgit"};
-        String[] branches = new String[]{ "tests/getSubmodules", "master" };
+        List<Object[]> testArgs = new ArrayList<>();
+        String[] gitImplementations = new String[]{"git" , "jgit", "jgitapache"};
+        String[] branches = new String[]{"tests/getSubmodules", "tests/notSubmodules", "master"};
         for (String implementation : gitImplementations) {
             for (String initialBranch : branches) {
-                Object[] arg = {implementation, initialBranch};
-                testArgs.add(arg);
+                for (String finalBranch : branches) {
+                    Object[] arg = {implementation, initialBranch, finalBranch};
+                    testArgs.add(arg);
+                }
             }
         }
         return testArgs;
@@ -84,16 +77,30 @@ public class SubmoduleTest {
     @Test
     public void checkoutBranch() throws Exception {
         ws.cloneMirror();
-        ws.getGitClient().checkoutBranch(initialBranch, "origin/" + initialBranch);
+        GitClient git = ws.getGitClient();
+        git.checkoutBranch(initialBranch, "origin/" + initialBranch);
+        // git.submoduleInit();
+        // git.submoduleUpdate().ref(ws.repoPath()).execute();
+        git.checkoutBranch(finalBranch, "origin/" + finalBranch);
+        // git.submoduleInit();
+        // git.submoduleUpdate().execute();
     }
 
     @Test
     public void checkout() throws Exception {
         ws.cloneMirror();
-        /* Temporary skip for master */
-        if (initialBranch.equals("master")) {
-            return;
+        GitClient git = ws.getGitClient();
+        if (!initialBranch.equals("master")) {
+            /* Fails to checkout master branch, since it already exists */
+            git.checkout("origin/" + initialBranch, initialBranch);
         }
-        ws.getGitClient().checkout("origin/" + initialBranch, initialBranch);
+        // git.submoduleInit();
+        // git.submoduleUpdate().execute();
+        if (!initialBranch.equals(finalBranch) && !finalBranch.equals("master")) {
+            ObjectId branchId = git.revParse("origin/" + finalBranch);
+            git.checkout(branchId.getName(), finalBranch);
+        }
+        // git.submoduleInit();
+        // git.submoduleUpdate().execute();
     }
 }
