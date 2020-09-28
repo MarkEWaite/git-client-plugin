e73252e6356806cda319f0a5191f31393482cc8d
Mark Waite <mark.earl.waite@gmail.com>
2020-09-27 20:30:31 -0600

    Skip JMH benchmarks based on commit message content
    The Jenkinsfile already requires a commit message that includes the word
    "benchmark" or "Benchmark".  This change makes the unit test have the
    same condition.  JMH benchmarks will be run in CI if and only if the word
    "[Bb]enchmark" is included in one of the last 3 messages.
    
    Write empty JMH report file for archiver.

--- src/test/java/jmh/benchmark/BenchmarkRunner.java
+++ src/test/java/jmh/benchmark/BenchmarkRunner.java
@@ -1,22 +1,63 @@
 package jmh.benchmark;
 
+import hudson.EnvVars;
+import hudson.model.TaskListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
 import jenkins.benchmark.jmh.BenchmarkFinder;
+
+import org.jenkinsci.plugins.gitclient.ChangelogCommand;
+import org.jenkinsci.plugins.gitclient.Git;
+import org.jenkinsci.plugins.gitclient.GitClient;
+import org.jenkinsci.plugins.gitclient.JGitAPIImpl;
+
 import org.junit.Test;
+
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * A runner class which finds benchmark tests annotated with @JmhBenchmark and launches them with the selected options
  * provided by JMH
  */
 public class BenchmarkRunner {
+    /**
+     * Return true if benchmarks should be run.
+     *
+     * If one of the last 3 commits includes the word "Benchmark" or
+     * "benchmark", then benchmarks should run.  Otherwise, do not run
+     * benchmarks.
+     *
+     * See Jenkinsfile for the same logic.
+     *
+     * @return true if benchmarks should be run
+     */
+    private boolean shouldRunBenchmarks() throws Exception {
+        String defaultValue = "not set by surefire.profile environment variable";
+        String mavenProfile = System.getProperty("surefire.profile", defaultValue);
+        System.out.println("**** Maven profile is '" + mavenProfile + "'");
+        GitClient defaultClient = Git.with(TaskListener.NULL, new EnvVars()).in(new File(".")).using("jgit").getClient();
+        StringWriter changelogWriter = new StringWriter();
+        defaultClient.changelog().includes("HEAD").max(3).to(changelogWriter).execute();
+        return mavenProfile.equals("something-unexpected") || changelogWriter.toString().matches("[Bb]enchmark");
+    }
+
     @Test
     public void runJmhBenchmarks() throws Exception {
+        if (!shouldRunBenchmarks()) {
+            String msg = "Benchmarks intentionally skipped";
+            Files.write(Paths.get("jmh-report.json"), msg.getBytes());
+            return;
+        }
         ChainedOptionsBuilder options = new OptionsBuilder()
                 .mode(Mode.AverageTime) // Performance metric is Average time (ms per operation)
                 .warmupIterations(1) // Used to warm JVM before executing benchmark tests
